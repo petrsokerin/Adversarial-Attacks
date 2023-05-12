3c159f90aef17ad7992b04956cc12c8d5e149953
PS_sc <petya4343@mail.ru>
2023-05-12 14:51:22 +0300

    added configs

---
+++ attack_config.py
@@ -0,0 +1,78 @@
+import pickle
+import copy
+import warnings
+warnings.filterwarnings('ignore')
+
+import hydra
+from omegaconf import DictConfig
+
+from tqdm.auto import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+
+from utils.data import load_Ford_A, transform_Ford_A, MyDataset
+from models.models import LSTM_net
+
+from utils.attacks import ifgsm_procedure
+from utils.utils import save_experiment, load_disc_model
+from utils.discrim_attack import HideAttackExp
+from utils.config import get_attack, load_disc_config
+from utils.attacks import (fgsm_disc_attack, fgsm_attack, fgsm_reg_attack, 
+simba_binary, simba_binary_reg, simba_binary_disc_reg)
+
+
+
+@hydra.main(config_path='config', config_name='experiment_run', version_base=None)
+def main(cfg: DictConfig):
+    if cfg['dataset'] == 'Ford_A':
+        X_train, X_test, y_train, y_test = load_Ford_A()
+        X_train, X_test, y_train, y_test = transform_Ford_A(X_train, X_test, y_train, y_test)
+  
+    test_loader = DataLoader(
+        MyDataset(X_test, y_test), 
+        batch_size=cfg['batch_size'] , 
+        shuffle=False
+        )
+
+    criterion = torch.nn.BCELoss()
+    n_objects = y_test.shape[0]
+    device= torch.device(cfg['cuda'] if torch.cuda.is_available() else 'cpu')
+    attack_func = get_attack(cfg['attack_type'])
+
+    disc_model_check = load_disc_model(model_id=cfg['disc_check_params']['model_id'], 
+                            path=cfg['disc_path'], 
+                            model_name=cfg['disc_check_params']['model_name'], 
+                            device=device)
+
+    for alpha in tqdm(cfg['alphas']):
+
+        attack_params = dict()
+
+        if 'reg' in cfg['attack_type'] :
+            attack_params['alpha'] = alpha
+
+        elif 'disc' in cfg['attack_type']:
+            attack_params['alpha'] = alpha
+            attack_params['disc_models'] = load_disc_config(cfg['disc_path'], device, cfg['list_reg_model_params'])  
+
+        model = LSTM_net(hidden_dim = 50, n_layers = 1, output_dim = 1, dropout=0.0).to(device)
+        model_path = cfg['model_folder'] + f'model_{cfg["model_id_attack"]}_FordA.pth'
+        model.load_state_dict(copy.deepcopy(torch.load(model_path)))
+
+
+
+        aa_res_df, rej_curves_dict = ifgsm_procedure(model=model, loader=test_loader, criterion=criterion,
+                                                    attack_func=attack_func, attack_params=attack_params,
+                                                    eps_params=cfg['eps_params'], n_steps=cfg['n_iterations'],
+                                                    n_objects=n_objects, train_mode=cfg['train_mode'],
+                                                    disc_model=disc_model_check)
+
+        save_experiment( aa_res_df, rej_curves_dict, path=cfg['save_path'], dataset=cfg["dataset"], model_id=cfg["model_id_attack"], alpha=alpha)
+        
+
+if __name__=='__main__':
+    main()
+
+
+
