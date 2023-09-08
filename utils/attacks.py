cf2eebbd82a6a6a138094896f1d0f2afcf3ecfd2
PS_sc <petya4343@mail.ru>
2023-09-08 16:52:53 +0300

    drop prints

--- utils/attacks.py
+++ utils/attacks.py
@@ -80,12 +80,12 @@ def fgsm_attack(model, loss_func, x, y_true, eps):
 
     return x_adv
 
-def deepfool_attack(model, loss_func, x, y_true, eps):
+def deepfool_attack(model, loss_func, x, y_true, eps, e=0.00001):
 
     y_pred = model(x, use_sigmoid=False, use_tanh=True)
     grad_ = torch.autograd.grad(torch.sum(y_pred), x, retain_graph=True)[0]
-    grad_norm =  torch.linalg.norm(grad_, dim=(1, 2))
-    coef_ = eps * y_pred * grad_norm.reshape(-1, 1)
+    grad_norm =  torch.linalg.norm(grad_, dim=(1, 2)) ** 2
+    coef_ = eps * y_pred / (grad_norm.reshape(-1, 1) + e)
 
     coef_ = coef_.unsqueeze(1).repeat(1, 50, 1)    
     perturb = - coef_ * grad_ 
@@ -106,21 +106,23 @@ def fgsm_reg_attack(model, loss_func, x, y_true, eps, alpha):
 
     return x_adv
 
-def fgsm_disc_attack(model, loss_func, x, y_true, eps: float, alpha: float, disc_models: List):
+def fgsm_disc_attack(model, loss_func, x, y_true, eps: float, alpha: float, disc_models: List, train_mode=False):
 
     y_pred = model(x)
     loss_val = loss_func(y_pred, y_true)
-    # grad_loss = torch.autograd.grad(loss_val, x, retain_graph=True)[0]
-    reg_value = reg_disc(x, alpha, disc_models)
-    # grad_reg = torch.autograd.grad(reg_value, x, retain_graph=True)[0]
+    grad_loss = torch.autograd.grad(loss_val, x, retain_graph=True)[0]
+    reg_value = reg_disc(x, alpha, disc_models, train_mode=train_mode)
+    grad_reg = torch.autograd.grad(reg_value, x, retain_graph=True)[0]
+
+    # print(grad_reg)
 
     # print('norm_grad_reg', torch.linalg.norm(grad_reg))
     # print('norm_grad_loss', torch.linalg.norm(grad_loss))
 
-    # grad_ = grad_loss - grad_reg
+    grad_ = grad_loss - grad_reg
 
-    loss = loss_val - reg_value
-    grad_ = torch.autograd.grad(loss_val, x, retain_graph=True)[0]
+    # loss = loss_val - reg_value
+    # grad_ = torch.autograd.grad(loss_val, x, retain_graph=True)[0]
 
     x_adv = x.data + eps * torch.sign(grad_)
 
@@ -136,14 +138,14 @@ def reg_neigh(x, alpha):
     reg_value = alpha * torch.mean(reg_value)
     return reg_value
 
-def reg_disc(x, alpha: float, disc_models: List):
+def reg_disc(x, alpha: float, disc_models: List, train_mode=False):
     n_models = len(disc_models)
     reg_value = 0
     for d_model in disc_models:
-
-        req_grad(d_model, state=False)
-        d_model.eval()
-        reg_value = reg_value + torch.mean(torch.log(F.sigmoid(d_model(x))))
+        d_model.train(True)
+        req_grad(d_model, state=True)
+        model_output = torch.mean(torch.log(F.sigmoid(d_model(x))))
+        reg_value = reg_value + model_output
 
     reg_value = alpha* reg_value / n_models
     return reg_value
